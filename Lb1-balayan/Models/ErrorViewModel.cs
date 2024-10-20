a1ae5d3a1918f13023e2bb5e7ed865bb8922245b
Ангелина Балаян <bangel28@mail.ru>
2024-10-20 19:07:23 +0300

    Добавьте файлы проекта.

---
+++ Lb1-balayan/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Lb1_balayan.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
